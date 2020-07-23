08afff2527f66de6c418f92bbdaab7451e5937ec
Adarsh-NP <f20180806@goa.bits-pilani.ac.in>
2020-07-23 15:34:00 +0530

    added validation

--- APIs/routes/product.js
+++ APIs/routes/product.js
@@ -6,10 +6,25 @@ const router = express.Router()
 const Product = require('../models/product')
 
 router.get('/', (req, res, next)=>{
-    Product.find().exec()
+    Product.find()
+    .select('name pride_id')
+    .exec()
     .then(docs=> {
-        console.log(docs);
-        res.status(200).json(docs)
+        const response = {
+            count: docs.length,
+            products: docs.map (doc => {
+                return {
+                    name: doc.name,
+                    price: doc.price,
+                    _id: doc._id,
+                    request : {
+                        type: 'GET',
+                        url: 'http://localhost:5000/products/' + doc._id
+                    }
+                }
+            })
+        }
+        res.status(200).json(response)
     })
     .catch(err => {
         console.log(err)
@@ -26,12 +41,22 @@ router.post('/', (req, res, next)=> {
         name: req.body.name,
         price: req.body.price
     })
-    product.save().then(result => {
-        console.log(result);
-        res.status(201).json({
-            message: 'handling post requests to /products',
-            createdProduct: result 
-        })
+    product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Created product successfully",
+        createdProduct: {
+            name: result.name,
+            price: result.price,
+            _id: result._id,
+            request: {
+                type: 'GET',
+                url: "http://localhost:5000/products/" + result._id
+            }
+        }
+      });
     })
     .catch(err => {
         console.log(err)
